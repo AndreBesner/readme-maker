0eeb10bd796f6227dd3d285308b1cbf6ab03334b
Andre Besner <besner.andre@gmail.com>
2023-05-07 20:50:54 -0400

    finish questions array

--- Develop/index.js
+++ Develop/index.js
@@ -3,7 +3,53 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Please enter a description of your project:',
+      },
+      {
+        type: 'input',
+        name: 'install-instruction',
+        message: 'Please describe any additional installation steps for your project:',
+      },
+      {
+        type: 'input',
+        name: 'usage-information',
+        message: 'Please enter any additional usage information for your project:',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Please enter any additional ways to contribute to this project:',
+      },
+      {
+        type: 'input',
+        name: 'test-instructions',
+        message: 'Please enter any additional testing instructions:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        choices: ['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3', 'None']
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Please enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Please enter your email:',
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
